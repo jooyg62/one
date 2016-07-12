281cec4f60c4039cb12abc4e6430c41c49d2c8fd
JangGyoo Seo <jooyg62@naver.com>
2016-07-12 15:43:44 +0900

    added print line

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("나 혼자 연습");
+		System.out.println("create hotfix branch");
 
 	}
 
